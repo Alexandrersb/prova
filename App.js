4c05bb8551c0a4485270c2b48b03f35b0540e36a
alexandre_r_barbosa <alexandreramossbar@gmail.com>
2023-04-25 21:58:28 -0300

    teste

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {useEffect} from "react";
+import DatabaseInit from "./src/database/DatabaseInit";
+import CadastroLocatario from "./src/components/CadastroLocatario";
+import CadastroImovel from "./src/components/CadastroImovel";
+import {ListarImovel} from "./src/components/ListarImovel";
+import {ListarLocatario} from "./src/components/ListarLocatario";
+import {HomeScreen} from "./src/components/Home";
+
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+
+  useEffect(() => {
+    new DatabaseInit()
+  },[])
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Cadastrar Locatário" component={CadastroLocatario} />
+        <Stack.Screen name="Cadastrar Imóvel" component={CadastroImovel} />
+          <Stack.Screen name="Listar Imóvel" component={ListarImovel} />
+          <Stack.Screen name="Listar Locatário" component={ListarLocatario} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
